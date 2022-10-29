540ff65220f09bee70ce049029fd2ddcfe93991e
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-10-29 11:19:07 +0200

    feat : add res to manage response to the server

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const http = require('http')
 const port = 3500
 
 const server = http.createServer((req, res)=>{
-    res.end('Hello Backend')
+    res.setHeader('content-type', 'text/html')
+    res.write(`<html>
+        <header>
+            <title>Node Page</title>
+        </header>
+        <body>
+            <h1>
+                Hello backend
+            </h1>
+        </body>
+    </html>`)
+    res.end()
     console.log(req.url, req.method, req.headers);
+   
 }) 
 
 server.listen(port, ()=>console.log(`server is running to port http://localhost:${port}`))
