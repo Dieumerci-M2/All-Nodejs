955f0bb1367640f6e19fb6c91e70c23a2d52e813
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-10-29 10:15:09 +0200

    refactor : filter req method

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3500
 
 const server = http.createServer((req, res)=>{
     res.end('Hello Backend')
+    console.log(req.url, req.method, req.headers);
 }) 
 
 server.listen(port, ()=>console.log(`server is running to port http://localhost:${port}`))
