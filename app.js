28c33c13298c3314fbd1f81baa087e2316587099
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-02 12:51:08 +0100

    feat : begin with express dependancy

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
 const http = require('http')
+const express = require('express')
 
+const app = express();
 const port = 3500
 
-const server = http.createServer((req, res)=>{
-    res.setHeader('content-type', 'text/html')
-    if(req.url === '/'){
-        res.write(`<html>
-        <header>
-            <title>Node Page</title>
-        </header>
-        <body>
-            <form action= '/message' method='POST'>
-                <input type= 'texte' name='message'>
-                <button type='submit'>Envoyer</button>
-            </form>
-        </body>
-    </html>`)
-    return res.end()
-    }
-    else if(req.url === '/message'){
-        res.write(`<html>
-        <header>
-            <title>Node Page</title>
-        </header>
-        <body>
-            <form action= '/'>
-                <div class= 'message'></div>
-                <button type='submit'>add new message</button>
-            </form>
-        </body>
-    </html>`)
-    return res.end()
-    }
-  
-    console.log(req.url, req.method, req.headers);
+const server = http.createServer((app)=>{
    
 }) 
 
